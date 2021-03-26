dd6a88fc8ea6bf77b5fc8c1b3a1180e7834d105c
ardaaylmazz <arday2431@gmail.com>
2021-03-26 11:24:48 +0300

    first-files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style.css";
+import { useState } from "react";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const inputHandle = (e) => {
+    setInput(e.target.value);
+  };
+
+  const clickHandle = () => {
+    if (input === "") {
+      alert("Ne yapacağına karar ver!");
+    } else if (todos.find((todo) => todo.name === input)) {
+      alert("Bunu zaten yapacaksın!");
+      setInput("");
+    } else {
+      const newTodo = {
+        name: input,
+        done: false,
+      };
+      setTodos([...todos, newTodo]);
+      setInput("");
+    }
+  };
+
+  const removeTodo = (todoKey) => {
+    setTodos(todos.filter((todo, key) => key !== todoKey));
+  };
+
+  const toggleTodo = (todoKey) => {
+    setTodos(
+      todos.map((todo, key) => {
+        if (key === todoKey) {
+          todo.done = !todo.done;
+        }
+        return todo;
+      })
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input
+          type="text"
+          value={input}
+          onChange={inputHandle}
+          placeholder="Ne yapacaksın?"
+          autoFocus
+        />
+        <button onClick={clickHandle}>Ekle</button>
+      </form>
+      <ul>
+        {todos.map((todo, key) => (
+          <li key={key} className={todo.done ? "done" : ""}>
+            <span onClick={() => toggleTodo(key)}>{todo.name}</span>
+            <button onClick={() => removeTodo(key)}>Sil</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
